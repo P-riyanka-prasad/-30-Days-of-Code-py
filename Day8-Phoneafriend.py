df3d56fdf88fdb75d81390a83eb6f895b9812ee7
priyanka prasad <59612128+P-riyanka-prasad@users.noreply.github.com>
2020-09-21 15:19:14 +0530

    Create Day8-Phoneafriend.py

---
+++ Day8-Phoneafriend.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+p=int(input())
+mydict={}
+for i in range(p):
+    data=list(input().rstrip().split())
+    mydict[data[0]]=data[1]
+mydict_keys=mydict.keys()
+while True:
+    try:
+        name=input()
+        if name in mydict_keys:
+            print(name+"="+mydict[name])
+        else:
+            print("Not found")
+    except:
+        break
